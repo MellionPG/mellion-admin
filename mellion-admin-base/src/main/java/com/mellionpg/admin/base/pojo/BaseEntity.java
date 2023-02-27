084d92a8532776ee7f786d04b142e75436979714
MellionPG <mellionpg@outlook.com>
2023-02-27 12:14:13 +0800

    feat: 添加测试样例TestDemo，测试基础CRUD功能

---
+++ mellion-admin-base/src/main/java/com/mellionpg/admin/base/pojo/BaseEntity.java
@@ -0,0 +1,75 @@
+package com.mellionpg.admin.base.pojo;
+
+import com.baomidou.mybatisplus.annotation.*;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Null;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+/**
+ * 基础entity实体类
+ *
+ * @author MellionPG
+ * @date 2023/2/27 11:05
+ **/
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BaseEntity implements Serializable {
+
+    /**
+     * 主键ID
+     */
+    @ApiModelProperty("主键ID")
+    @TableId(value = "id", type = IdType.ASSIGN_ID)
+    private Long id;
+
+    /**
+     * 创建者
+     */
+    @ApiModelProperty(value = "创建者", hidden = true)
+    @TableField(fill = FieldFill.INSERT, updateStrategy = FieldStrategy.NEVER)
+    @Null
+    private String creator;
+
+    /**
+     * 创建时间
+     */
+    @ApiModelProperty(value = "创建时间", hidden = true)
+    @TableField(updateStrategy = FieldStrategy.NEVER)
+    @Null
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private LocalDateTime createTime;
+
+    /**
+     * 更新者
+     */
+    @ApiModelProperty(value = "更新者", hidden = true)
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    @Null
+    private String updater;
+
+    /**
+     * 更新时间
+     */
+    @ApiModelProperty(value = "更新时间", hidden = true)
+    @TableField(updateStrategy = FieldStrategy.NEVER)
+    @Null
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private LocalDateTime updateTime;
+
+    /**
+     * 逻辑删除标志（'1'已删除 '0'未删除）
+     */
+    @ApiModelProperty(value = "逻辑删除标志", hidden = true)
+    @TableLogic
+    @TableField(updateStrategy = FieldStrategy.NEVER)
+    @Null
+    private Integer deleted;
+
+}
---
+++ mellion-admin-server/src/main/java/com/mellionpg/admin/server/test/controller/TestDemoController.java
@@ -0,0 +1,94 @@
+package com.mellionpg.admin.server.test.controller;
+
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.mellionpg.admin.base.pojo.PageParam;
+import com.mellionpg.admin.base.pojo.ResponseResult;
+import com.mellionpg.admin.server.test.entity.TTestDemo;
+import com.mellionpg.admin.server.test.service.TestDemoService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+/**
+ * 测试样例控制器
+ *
+ * @author MellionPG
+ * @date 2023/2/27 10:48
+ **/
+@RestController
+@RequestMapping("/test/demo")
+@Validated
+@Api(tags = "测试样例接口")
+@RequiredArgsConstructor
+public class TestDemoController {
+
+    /**
+     * 测试样例服务
+     */
+    private final TestDemoService testDemoService;
+
+    /**
+     * 根据id获取测试样例详情
+     *
+     * @param id 主键
+     * @return 测试样例详情
+     */
+    @GetMapping
+    @ApiOperation("根据id获取测试样例详情")
+    public ResponseResult<TTestDemo> getById(@RequestParam Long id) {
+        return ResponseResult.success(testDemoService.getById(id));
+    }
+
+    /**
+     * 新增测试样例
+     *
+     * @param tTestDemo 测试样例
+     * @return 测试样例
+     */
+    @PostMapping
+    @ApiOperation("新增测试样例")
+    public ResponseResult<TTestDemo> add(@RequestBody @Valid TTestDemo tTestDemo) {
+        return ResponseResult.success(testDemoService.add(tTestDemo));
+    }
+
+    /**
+     * 根据id修改测试样例
+     *
+     * @param tTestDemo 测试样例
+     * @return 测试样例
+     */
+    @PutMapping
+    @ApiOperation("根据id修改测试样例")
+    public ResponseResult<TTestDemo> editById(@RequestBody @Valid TTestDemo tTestDemo) {
+        return ResponseResult.success(testDemoService.editById(tTestDemo));
+    }
+
+    /**
+     * 根据id删除测试样例
+     *
+     * @param id 主键
+     * @return 删除状态
+     */
+    @DeleteMapping
+    @ApiOperation("根据id删除测试样例")
+    public ResponseResult<Boolean> deleteById(@RequestParam Long id) {
+        return ResponseResult.success(testDemoService.deleteById(id));
+    }
+
+    /**
+     * 分页获取测试样例
+     *
+     * @param pageParam 分页参数
+     * @return 分页后的测试样例
+     */
+    @GetMapping("/page")
+    @ApiOperation("分页获取测试样例")
+    public ResponseResult<Page<TTestDemo>> getPage(@RequestParam @Valid PageParam pageParam) {
+        return ResponseResult.success(testDemoService.getPage(pageParam));
+    }
+
+}
