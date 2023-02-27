66d40cfe0e4352e5241f995ccb2c4d314009acc6
MellionPG <mellionpg@outlook.com>
2023-02-27 11:04:51 +0800

    feat: 添加MybatisPlus分页配置，初始化MybatisPlus字段自动注入处理类

---
+++ mellion-admin-base/src/main/java/com/mellionpg/admin/base/config/MybatisPlusConfig.java
@@ -0,0 +1,26 @@
+package com.mellionpg.admin.base.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * MybatisPlus配置类
+ *
+ * @author MellionPG
+ * @date 2023/2/27 11:02
+ **/
+@Configuration
+public class MybatisPlusConfig {
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+        // 配置分页拦截器
+        mybatisPlusInterceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return mybatisPlusInterceptor;
+    }
+
+}
