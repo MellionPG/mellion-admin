53b68ed7f2f4843d55b03d0c777187c2e17e5faf
MellionPG <mellionpg@outlook.com>
2023-02-27 10:44:29 +0800

    feat: 配置CORS解决请求跨域问题

---
+++ mellion-admin-base/src/main/java/com/mellionpg/admin/base/config/CorsConfig.java
@@ -0,0 +1,32 @@
+package com.mellionpg.admin.base.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * 跨域cors配置类
+ *
+ * @author MellionPG
+ * @date 2023/2/27 10:42
+ **/
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOriginPattern("*");
+        configuration.addAllowedMethod("*");
+        configuration.addAllowedHeader("*");
+        // 允许携带cookie跨域
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return new CorsFilter(source);
+    }
+
+}
