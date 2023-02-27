454509a0ec6e88ec1a47c795732b76e91ca29f0c
MellionPG <mellionpg@outlook.com>
2023-02-27 10:24:03 +0800

    feat: 封装请求响应结果ResponseResult，添加响应状态枚举类

---
+++ mellion-admin-base/src/main/java/com/mellionpg/admin/base/pojo/ResponseResult.java
@@ -0,0 +1,162 @@
+package com.mellionpg.admin.base.pojo;
+
+import com.mellionpg.admin.base.enums.BaseResponseStatus;
+import com.mellionpg.admin.base.interfaces.ResponseStatus;
+import lombok.Builder;
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * 统一响应结果对象，封装响应结果
+ *
+ * @param <T> 响应数据的类型
+ * @author MellionPG
+ * @date 2023/2/27 9:58
+ **/
+@Data
+@Builder
+public class ResponseResult<T> implements Serializable {
+
+    /**
+     * 响应状态码
+     */
+    private Integer code;
+
+    /**
+     * 响应消息
+     */
+    private String msg;
+
+    /**
+     * 响应具体数据
+     */
+    private T data;
+
+    /**
+     * 封装请求成功但不附带数据的结果对象
+     *
+     * @param <T> 数据对应的类
+     * @return 响应结果
+     */
+    public static <T> ResponseResult<T> success() {
+        return success(null);
+    }
+
+    /**
+     * 封装请求成功的结果对象
+     *
+     * @param data 具体数据
+     * @param <T>  数据对应的类
+     * @return 响应结果
+     */
+    public static <T> ResponseResult<T> success(T data) {
+        return ResponseResult.<T>builder().code(BaseResponseStatus.SUCCESS.getCode())
+                .msg(BaseResponseStatus.SUCCESS.getMsg())
+                .data(data)
+                .build();
+    }
+
+    /**
+     * 封装请求失败的结果对象：默认code、msg，无data
+     *
+     * @param <T> 数据对应的类
+     * @return 响应结果
+     */
+    public static <T> ResponseResult<T> failure() {
+        return ResponseResult.<T>builder().code(BaseResponseStatus.FAILURE.getCode())
+                .msg(BaseResponseStatus.FAILURE.getMsg())
+                .data(null)
+                .build();
+    }
+
+    /**
+     * 封装请求失败的结果对象：默认code，指定msg，无data
+     *
+     * @param msg 响应提示消息
+     * @param <T> 数据对应的类
+     * @return 响应结果
+     */
+    public static <T> ResponseResult<T> failure(String msg) {
+        return ResponseResult.<T>builder().code(BaseResponseStatus.FAILURE.getCode())
+                .msg(msg)
+                .data(null)
+                .build();
+    }
+
+    /**
+     * 封装请求失败的结果对象：指定code、msg，无data
+     *
+     * @param code 响应状态码
+     * @param msg  响应提示消息
+     * @param <T>  数据对应的类
+     * @return 响应结果
+     */
+    public static <T> ResponseResult<T> failure(Integer code, String msg) {
+        return ResponseResult.<T>builder().code(code)
+                .msg(msg)
+                .data(null)
+                .build();
+    }
+
+    /**
+     * 封装请求失败的结果对象：指定code、msg(枚举类)，无data
+     *
+     * @param responseStatus 响应状态枚举类
+     * @param <T>            数据对应的类
+     * @return 响应结果
+     */
+    public static <T> ResponseResult<T> failure(ResponseStatus responseStatus) {
+        return ResponseResult.<T>builder().code(responseStatus.getCode())
+                .msg(responseStatus.getMsg())
+                .data(null)
+                .build();
+    }
+
+    /**
+     * 封装请求失败的结果对象：默认code，指定msg、data
+     *
+     * @param msg  响应提示消息
+     * @param data 错误数据
+     * @param <T>  数据对应的类
+     * @return 响应结果
+     */
+    public static <T> ResponseResult<T> failure(String msg, T data) {
+        return ResponseResult.<T>builder().code(BaseResponseStatus.FAILURE.getCode())
+                .msg(msg)
+                .data(data)
+                .build();
+    }
+
+    /**
+     * 封装请求失败的结果对象：指定code、msg、data
+     *
+     * @param code 响应状态码
+     * @param msg  响应提示消息
+     * @param data 错误数据
+     * @param <T>  数据对应的类
+     * @return 响应结果
+     */
+    public static <T> ResponseResult<T> failure(Integer code, String msg, T data) {
+        return ResponseResult.<T>builder().code(code)
+                .msg(msg)
+                .data(data)
+                .build();
+    }
+
+    /**
+     * 封装请求失败的结果对象：指定code(枚举类)、msg(枚举类)、data
+     *
+     * @param responseStatus 响应状态枚举类
+     * @param data           错误数据
+     * @param <T>            数据对应的类
+     * @return 响应结果
+     */
+    public static <T> ResponseResult<T> failure(ResponseStatus responseStatus, T data) {
+        return ResponseResult.<T>builder().code(responseStatus.getCode())
+                .msg(responseStatus.getMsg())
+                .data(data)
+                .build();
+    }
+
+}
