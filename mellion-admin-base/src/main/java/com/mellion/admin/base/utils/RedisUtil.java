8fa08df4f27d9f90ca9c530d4543ceeda75690af
MellionPG <mellionpg@outlook.com>
2023-03-07 17:47:25 +0800

    feat: 在RedisUtil工具类上添加存储Object类型的方法

--- mellion-admin-base/src/main/java/com/mellion/admin/base/utils/RedisUtil.java
+++ mellion-admin-base/src/main/java/com/mellion/admin/base/utils/RedisUtil.java
@@ -1,6 +1,9 @@
 package com.mellion.admin.base.utils;
 
 import cn.hutool.extra.spring.SpringUtil;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
@@ -11,6 +14,7 @@ import java.util.concurrent.TimeUnit;
  * @date 2023/2/27 14:51
  **/
 @Component
+@Slf4j
 public class RedisUtil {
 
     /**
@@ -53,6 +57,41 @@ public class RedisUtil {
         } else {
             redisTemplate.opsForValue().set(key, value);
         }
+    }
+
+    /**
+     * 将值放入缓存
+     *
+     * @param key   键
+     * @param value 值
+     */
+    public static void set(String key, Object value) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json = null;
+        try {
+            json = objectMapper.writeValueAsString(value);
+        } catch (JsonProcessingException e) {
+            log.error("json转换异常：{}", e.getMessage(), e);
+        }
+        set(key, json);
+    }
+
+    /**
+     * 将值放入缓存并设置时间
+     *
+     * @param key   键
+     * @param value 值
+     * @param time  时间(秒) -1为无期限
+     */
+    public static void set(String key, Object value, long time) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json = null;
+        try {
+            json = objectMapper.writeValueAsString(value);
+        } catch (JsonProcessingException e) {
+            log.error("json转换异常：{}", e.getMessage(), e);
+        }
+        set(key, json, time);
     }
 
 }
