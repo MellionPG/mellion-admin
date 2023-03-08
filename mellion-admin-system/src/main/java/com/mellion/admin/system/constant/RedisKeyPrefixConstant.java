a1af12b055027548e374eacfde71ec3c740fb3a4
MellionPG <mellionpg@outlook.com>
2023-03-08 19:20:02 +0800

    feat: 添加Redis键的前缀字符串常量类

---
+++ mellion-admin-system/src/main/java/com/mellion/admin/system/constant/RedisKeyPrefixConstant.java
@@ -0,0 +1,15 @@
+package com.mellion.admin.system.constant;
+
+/**
+ * Redis键的前缀字符串常量
+ *
+ * @author MellionPG
+ * @date 2023/3/8 19:16
+ **/
+public class RedisKeyPrefixConstant {
+
+    public final static String SYSTEM_USER_TOKEN = "system:user:token:";
+
+    public final static String SYSTEM_USER_PERMISSION = "system:user:permission:";
+
+}
